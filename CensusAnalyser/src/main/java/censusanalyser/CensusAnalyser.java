4969c130b71eee854fd5b198657a640c4546e747
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-09 11:02:17 +0530

    refactor the code to close the file

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -12,23 +12,28 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
+
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+
+        CsvToBean<IndiaCensusCSV> csvToBean;
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            csvToBean = csvToBeanBuilder.build();
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-            Iterable<IndiaCensusCSV> csvItrable=()->censusCSVIterator;
-            int numOfEntries=(int) StreamSupport.stream(csvItrable.spliterator(),false).count();
+            Iterable<IndiaCensusCSV> csvItrable = () -> censusCSVIterator;
+            int numOfEntries = (int) StreamSupport.stream(csvItrable.spliterator(), false).count();
             return numOfEntries;
+
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }catch (IllegalStateException e) {
+        } catch (IllegalStateException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
         }
+
+
     }
 }
