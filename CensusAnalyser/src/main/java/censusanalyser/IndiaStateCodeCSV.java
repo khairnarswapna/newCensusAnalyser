798b11d38f1941568b3d2fe35eb96d462d04c2d8
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-09 11:57:19 +0530

    add the IndiaStatecodeCsv Module

--- CensusAnalyser/src/main/java/censusanalyser/IndiaStateCodeCSV.java
+++ CensusAnalyser/src/main/java/censusanalyser/IndiaStateCodeCSV.java
@@ -1,4 +1,18 @@
 package censusanalyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class IndiaStateCodeCSV {
+    @CsvBindByName(column = "State Name", required = true)
+    public String state;
+    @CsvBindByName(column = "StateCode", required = true)
+    public String StateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaStateCodeCSV{" +
+                "state='" + state + '\'' +
+                ", StateCode='" + StateCode + '\'' +
+                '}';
+    }
 }
