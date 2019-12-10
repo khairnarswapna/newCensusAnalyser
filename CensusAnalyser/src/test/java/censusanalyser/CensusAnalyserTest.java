cc77b5c42fb08d004f75031518153f2e965881b6
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-10 17:28:32 +0530

    add:add:Testcase for IndiaStateCodeData With Incorrect Delimiter Should Throw Exception

--- CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
+++ CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -5,11 +5,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
+
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-    private static final String INDIAN__STATE_CSV_PATH="/home/admin142/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCode.csv";
+    private static final String INDIA__STATE_CSV_PATH="./src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_CODE_CSV_FILE_PATH="./src/main/resources/IndiaStateCode.csv";
+    private static final String  EMPTY_FILE_PATH="";
+    private static final String DELIMETERMISSING_CENCUSFILE="./src/test/resources/delimeterMissing.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -35,16 +40,47 @@ public class CensusAnalyserTest {
     @Test
     public void givenIndianStatCsv_shouldReturnexactCount() throws CensusAnalyserException {
         CensusAnalyser censusAnalyser=new CensusAnalyser();
-        int numofStateCode=censusAnalyser.loadStateCode(INDIAN__STATE_CSV_PATH);
+        int numofStateCode=censusAnalyser.loadStateCode(INDIA__STATE_CSV_PATH);
         Assert.assertEquals(37,numofStateCode);
     }
-
     @Test
-    public void  givenIndianCensusData_whensortedOnState_shouldreturnSortedResult() throws CensusAnalyserException {
-        CensusAnalyser censusAnalyser=new CensusAnalyser();
-        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-        String sortedCensusdata=censusAnalyser.getStateWiseSortedData(INDIA_CENSUS_CSV_FILE_PATH );
-        IndiaCensusCSV[] censusCSV=new Gson().fromJson(sortedCensusdata,IndiaCensusCSV[].class);
-        Assert.assertEquals("Andhra Pradesh",censusCSV[0].state);
+    public void givenIndianStateCodeCSV_WithWrongPath_ShouldThrowException()
+    {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadStateCode(WRONG_CODE_CSV_FILE_PATH);
+        }
+        catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    @Test
+    public void givenIndiaStateCodeData_WithIncorrectDelimiter_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData((DELIMETERMISSING_CENCUSFILE));
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.SOME_FILE_ISSUE, e.type);
+        }
+    }
+    @Test
+    public void  givenIndianCensusData_whensortedOnState_shouldreturnSortedResult() {
+
+        try {
+            CensusAnalyser censusAnalyser=new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            String sortedCensusData = censusAnalyser.getStateWiseSortedData(INDIA_CENSUS_CSV_FILE_PATH );
+            IndiaCensusCSV[] censusCSV=new Gson().fromJson(sortedCensusData,IndiaCensusCSV[].class);
+            Assert.assertEquals("Andhra Pradesh",censusCSV[0].state);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+
     }
 }
